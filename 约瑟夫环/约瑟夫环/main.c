0e69de88d197dff9d9a3e286338acab1bd129b07
guch8017 <guch8017@gmail.com>
2019-09-18 20:25:34 +0800

    更新readme

--- 约瑟夫环/约瑟夫环/main.c
+++ 约瑟夫环/约瑟夫环/main.c
@@ -13,6 +13,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char buff[10001];//保存输出
 
 struct node{
     int password;
@@ -37,6 +38,10 @@ int str2int(const char * str){
 
 pnode CreateNode(){
     pnode tmp = (pnode)malloc(sizeof(node));
+    if(!tmp){
+        printf("[E] 溢出错误:malloc分配内存失败。\n");
+        exit(0);
+    }
     tmp->next = NULL;
     return tmp;
 }
@@ -45,6 +50,10 @@ void Insert(pnode head, int password){
     static int i = 1;
     if(!head->next){
         head->next = (pnode)malloc(sizeof(node));
+        if(!head->next){
+            printf("[E] 溢出错误:malloc分配内存失败。\n");
+            exit(0);
+        }
         head->next->flag = 0;
         head->next->next = head->next;
         head->next->password = password;
@@ -56,6 +65,10 @@ void Insert(pnode head, int password){
         p=p->next;
     }
     p->next = (pnode)malloc(sizeof(node));
+    if(!p->next){
+        printf("[E] 溢出错误:malloc分配内存失败。\n");
+        exit(0);
+    }
     p->next->flag = 0;
     p->next->next = head->next;
     p->next->password = password;
@@ -64,20 +77,25 @@ void Insert(pnode head, int password){
 }
 
 int main(int argc, const char * argv[]) {
-    printf("%d\n",argc);
     int m,n;
+    FILE *fp;
+    fp = fopen("out.txt", "w+");
+    if(!fp){
+        printf("[E] IO错误:无法写入到文件out.txt\n");
+        return 0;
+    }
     if(argc < 3){
-        printf("[E] 参数错误:参数不足\n");
+        printf("[E] 参数错误:参数不足，期望：%d 实际：%d\n",2,argc-1);
         return 0;
     }
     m = str2int(argv[1]);//人数
     n = str2int(argv[2]);//停止报数
     if(m<=0 || n<=0){
-        printf("[E] 参数错误:参数输入错误\n");
+        printf("[E] 参数错误:参数输入错误，m与n应为正整数\n");
         return 0;
     }
     if(argc < 3 + n){
-        printf("[E] 参数错误:参数不足\n");
+        printf("[E] 参数错误:参数不足，期望：%d 实际：%d\n",2+n,argc-1);
         return 0;
     }else if(argc > 3 + n){
         printf("[W] 参数过多，多余参数将被舍弃\n");
@@ -88,7 +106,7 @@ int main(int argc, const char * argv[]) {
     for(int i=3;i<n+3;i++){
         int t = str2int(argv[i]);
         if(t<=0){
-            printf("[E] 参数错误:参数输入错误\n");
+            printf("[E] 参数错误:参数输入错误.第%d个：%s\n",i-1,argv[i]);
             return 0;
         }
         Insert(Head, t);
@@ -109,6 +127,7 @@ int main(int argc, const char * argv[]) {
             m = p->password;
             count=0;
             printf("%d,",p->id);
+            fprintf(fp, "%d ",p->id);
             n--;
         }
         p = p->next;
@@ -124,6 +143,7 @@ int main(int argc, const char * argv[]) {
             count = 0;
             n--;
             printf("%d,",p2->id);
+            fprintf(fp, "%d ",p2->id);
             p1->next = p2->next;
             free(p2);
             p2 = p1->next;
@@ -143,7 +163,7 @@ int main(int argc, const char * argv[]) {
     for(int i=0;i<n;i++){
         secret[i] = str2int(argv[i+3]);
         if(secret[i] <= 0){
-            printf("[E] 参数错误:参数输入错误\n");
+            printf("[E] 参数错误:参数输入错误.第%d个：%s\n",i+2,argv[i+3]);
             return 0;
         }
     }
@@ -163,6 +183,7 @@ int main(int argc, const char * argv[]) {
             flag[i]=1;
             n1--;
             printf("%d ",i+1);
+            fprintf(fp, "%d ",i+1);
         }
         i++;
         if(i>=n){
@@ -172,5 +193,5 @@ int main(int argc, const char * argv[]) {
     printf("\n");
 #endif
 //END OF 顺序表方案
-    
+    fclose(fp);
 }
