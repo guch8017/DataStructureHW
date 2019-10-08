e4c1c586a94634d9dae253aa5567b85f829307d1
guch8017 <38173059+guch8017@users.noreply.github.com>
2019-10-08 06:31:47 +0000

    2-4改为命令行控制文件输入输出

--- 2-4Bag/main.c
+++ 2-4Bag/main.c
@@ -48,17 +48,20 @@ void StackPop(pStack head){
     free(tmp);
 }
 
-void StackPrint(pStack head){
+void StackPrint(pStack head, FILE * outfp){
     putchar('(');
+    fputc(outfp,'(');
     pStack p = head;
     for(int i=0;i<head->i;i++){
         p = p->next;
         printf("%d,",p->num);
+        fprintf(outfp,"%d,",p->num);
     }
     printf("\b)\n");
+    fprintf(outfp,"\b)\n");
 }
 
-void Solution(){
+void Solution(FILE * outfp){
     pStack head = StackInit();
     int i=0;
     while(1){
@@ -79,7 +82,7 @@ void Solution(){
         }
         if(head->num + data[i] == T){
             StackPushBack(head, data[i], i);
-            StackPrint(head);
+            StackPrint(head,outfp);
             StackPop(head);
             i++;
             continue;
@@ -92,12 +95,56 @@ void Solution(){
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    T = atoi(argv[1]);
-    n = atoi(argv[2]);
+    int param[2] = {0};
+    for(int i=1;i<argc;i+=2){
+        if(!strcmp(argv[i], "-i")){
+            if(argc < i + 2){
+                printf("Option %s requires an argument\n",argv[i]);
+                return 0;
+            }
+            param[0] = i+1;
+        }else if(!strcmp(argv[i], "-o")){
+            if(argc < i + 2){
+                printf("Option %s requires an argument\n",argv[i]);
+                return 0;
+            }
+            param[1] = i+1;
+        }else if(!strcmp(argv[i], "-?")){
+            printf("decode [-i input file][-o output file]\n");
+            printf("\t-i\t\t#Input file. If not defined will use 'in.txt'\n");
+            printf("\t-o\t\t#Output file. If not defined will use 'out.txt'\n");
+            return 0;
+        }else{
+            printf("Unknown option: %s\nUse param -? to get help.\n",argv[i]);
+            return 0;
+        }
+    }
+    //char * s;//读入数据
+    FILE * infp;
+    FILE * outfp;
+    if(param[0]){
+        infp = fopen(argv[param[0]], "r");
+    }else{
+        infp = fopen("in.txt", "r");
+    }
+    if(param[1]){
+        outfp = fopen(argv[param[1]], "w");
+    }else{
+        outfp = fopen("out.txt", "w");
+    }
+    if(!infp){
+        printf("[E] 无法打开输入文件\n");
+        return 0;
+    }
+    if(!outfp){
+        printf("[E] 无法打开输出文件\n");
+        return 0;
+    }
+    fscanf(infp,"%d%d",&T,&n);
     data = (int *)malloc(sizeof(int) * n);
     for(int i=0;i<n;i++){
-        data[i] = atoi(argv[i+3]);
+        fscanf(infp,"%d",data + i);
     }
-    Solution();
+    Solution(outfp);
     return 0;
 }
